daa87204d057721f56daf7cc8d3336172dd6fba5
phil11971 <phil182@mail.ru>
2019-01-27 01:06:50 +0400

    add main.js to display a picture in a modal window

---
+++ app/js/main.js
@@ -0,0 +1,22 @@
+var wrap = $('#wrapper'),
+    modal = $('.cover, .modal, .content');
+
+$(document).ready(function(){
+   
+    $('#me').on('click', function(){
+        imageSource = $(this).attr('src');
+        $('#memodal').attr('src', imageSource);
+        modal.fadeIn();
+    }); 
+    
+    $('.modal').click(function() {
+        wrap.on('click', function(event) {
+          var select = $('.content');
+          if ($(event.target).closest(select).length)
+            return;
+          modal.fadeOut();
+          wrap.unbind('click');
+        });
+    });
+    
+});
