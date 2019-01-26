981fde99caee271cd6106605abc0233042772371
phil11971 <phil182@mail.ru>
2019-01-26 15:09:55 +0400

    add less

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,32 @@
 var gulp = require('gulp');
-var browserSync = require('browser-sync');
+var	browserSync = require('browser-sync');
+var	less = require('gulp-less');
+var	notify = require('gulp-notify');
+var	plumber = require('gulp-plumber');
 
-gulp.task('server', function(){
+gulp.task('less', function(){
+	return gulp.src('./app/less/main.less')
+		.pipe(plumber({
+			errorHandler: notify.onError(function(err){
+				return{
+					title: "less",
+					sound: false,
+					message: err.message
+				}
+			})
+		}))
+		.pipe(less())
+		.pipe(gulp.dest('./app/css'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('server', gulp.series('less', function(){
 	browserSync.init({
 		server: { baseDir: './app/'}
 	});
 	gulp.watch('app/**/*.html').on('change', browserSync.reload);
-});
+	gulp.watch('app/less/**/*.less', gulp.series('less'));
 
+}));
+
+gulp.task('default', gulp.series('server'));
